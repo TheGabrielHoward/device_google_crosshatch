df1d0e2b0fdf88aba26cdbb35137253969eea108
Gabriel Howard <j0s3ghoward@gmail.com>
2020-11-29 19:36:03 +0100

    bluecross: ElementOS Bringup

--- aosp_blueline.mk
+++ aosp_blueline.mk
@@ -14,40 +14,23 @@
 # limitations under the License.
 #
 
-#
 # All components inherited here go to system image
-#
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_system.mk)
 
-# Enable mainline checking
-# TODO(b/138706293): enable Enable mainline checking later
-#PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
-
-#
 # All components inherited here go to system_ext image
-#
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
-#
 # All components inherited here go to product image
-#
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
 
-#
 # All components inherited here go to vendor image
-#
-# TODO(b/136525499): move *_vendor.mk into the vendor makefile later
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 $(call inherit-product, device/google/crosshatch/device-blueline.mk)
-$(call inherit-product-if-exists, vendor/google_devices/crosshatch/proprietary/device-vendor.mk)
 
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
-
-# STOPSHIP deal with Qualcomm stuff later
-# PRODUCT_RESTRICT_VENDOR_FILES := all
 
 # Keep the VNDK APEX in /system partition for REL branches as these branches are
 # expected to have stable API/ABI surfaces.
@@ -55,8 +38,13 @@ ifneq (REL,$(PLATFORM_VERSION_CODENAME))
   PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
 endif
 
+# Inherit from ElementOS config
+$(call inherit-product, vendor/cafstyle/config/common_full_phone.mk)
+$(call inherit-product, vendor/google/blueline/blueline-vendor.mk)
+
+# Device Identifier
 PRODUCT_MANUFACTURER := Google
 PRODUCT_NAME := aosp_blueline
 PRODUCT_DEVICE := blueline
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on blueline
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Pixel 3
